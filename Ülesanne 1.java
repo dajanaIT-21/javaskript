89b34842dbfb8c2d7102456666b2d42d874be03b
Dajana Mähdi <98746095+dajanaIT-21@users.noreply.github.com>
2024-02-27 23:34:39 +0200

    Create Ülesanne 1.java

---
+++ Ülesanne 1.java
@@ -0,0 +1,12 @@
+// Dajana
+// 27.02.2024
+// Ülesanne 1
+
+console.log(typeof 4.2);           
+console.log(typeof 'Will Code 4 food'); 
+console.log(typeof "123");      
+console.log(typeof true);       
+console.log(typeof [1, 2, 3]);     
+console.log(typeof FALSE);         
+console.log(typeof 'true');       
+console.log(typeof undefined);    
