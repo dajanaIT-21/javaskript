cbaddf93376978b1d08057d6fe0cf69d5e11348e
Dajana Mähdi <98746095+dajanaIT-21@users.noreply.github.com>
2024-02-28 13:26:05 +0200

    Create Ülesanne 13.java

---
+++ Ülesanne 13.java
@@ -0,0 +1,9 @@
+// Dajana
+// 28.02.2024
+// Ülesanne 13
+
+const id = document.queryselector("p");
+id.innertext = "timm";
+id.setattribute("attr", "dajana mähdi"); 
+id.removeattribute("id");
+console.log(id.getattribute("attr"))
