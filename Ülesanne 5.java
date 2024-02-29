b9ba3f1f5f85b77d20fcfa3b38ba40267026ec1e
Dajana Mähdi <98746095+dajanaIT-21@users.noreply.github.com>
2024-02-29 19:32:40 +0200

    Update Ülesanne 5.java

--- Ülesanne 5.java
+++ Ülesanne 5.java
@@ -2,42 +2,60 @@
 // 27.02.2024
 // Ülesanne 5
 
-// Temperatuuri kontroll
-let temperatuur = 20; 
-if (temperatuur > 25) {
-    console.log("Väga kuum!");
-} else if (temperatuur >= 15 && temperatuur <= 25) {
-    console.log("Mõnus temperatuur");
-} else {
-    console.log("Jahe on!");
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JavaScripti kood</title>
+</head>
+<body>
+    <script>
+       
+        // Temperatuuri kontroll
+        let temperatuur = 20; 
+        if (temperatuur > 25) {
+            console.log("Väga kuum!");
+        } else if (temperatuur >= 15 && temperatuur <= 25) {
+            console.log("Mõnus temperatuur");
+        } else {
+            console.log("Jahe on!");
+        }
 
-// Kasutajanime kontroll
-let kasutajanimi = "admin"; 
-if (kasutajanimi === "admin") {
-    console.log("Tere, administraator!");
-} else {
-    console.log("Tere, külaline!");
-}
+        // Kasutajanime kontroll
+        let kasutajanimi = "admin"; 
+        if (kasutajanimi === "admin") {
+            console.log("Tere, administraator!");
+        } else {
+            console.log("Tere, külaline!");
+        }
 
-// Ürituse piletite hind
-let piletituup = "sooduspilet"; 
-let vanus = 25; 
+        // Ürituse piletite hind
+        let piletituup = "sooduspilet"; 
+        let vanus = 5; 
 
-if (piletituup === "täispilet") {
-    if (vanus < 18) {
-        console.log("Pileti hind: 10 eurot");
-    } else if (vanus >= 18 && vanus <= 64) {
-        console.log("Pileti hind: 20 eurot");
-    } else {
-        console.log("Pileti hind: 15 eurot");
-    }
-} else if (piletituup === "sooduspilet") {
-    if (vanus < 18 || vanus >= 65) {
-        console.log("Pileti hind: 8 eurot");
-    } else {
-        console.log("Pileti hind: 15 eurot");
-    }
-} else {
-    console.log("Vale piletitüüp!");
-}
+        if (piletituup === "täispilet") {
+            if (vanus < 18) {
+                console.log("Pileti hind: 10 eurot");
+            } else if (vanus >= 18 && vanus <= 64) {
+                console.log("Pileti hind: 20 eurot");
+            } else {
+                console.log("Pileti hind: 15 eurot");
+            }
+        } else if (piletituup === "sooduspilet") {
+            if (vanus < 18 || vanus >= 65) {
+                console.log("Pileti hind: 8 eurot");
+            } else {
+                console.log("Pileti hind: 15 eurot");
+            }
+        } else {
+            console.log("Vale piletitüüp!");
+        }
+
+
+
+
+
+    </script>
+</body>
+</html>
