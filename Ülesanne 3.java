b0f249ad4dd6bd2901ac07cc16ff0f805716f3b2
Dajana Mähdi <98746095+dajanaIT-21@users.noreply.github.com>
2024-02-29 19:32:14 +0200

    Update Ülesanne 3.java

--- Ülesanne 3.java
+++ Ülesanne 3.java
@@ -2,34 +2,52 @@
 // 27.02.2024
 // Ülesanne 3
 
-// Sõidu aeg ja kaugus
-let kaugus = 150; 
-let kiirus = 60; 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JavaScripti kood</title>
+</head>
+<body>
+    <script>
+        // Sõidu aeg ja kaugus
+        let kaugus = 150; 
+        let kiirus = 60; 
 
-// Arvutame sõidu aja (tundides)
-let soiduaeg = kaugus / kiirus;
-console.log("Sõidu aeg:", soiduaeg, "tundi");
+        // Arvutame sõidu aja (tundides)
+        let soiduaeg = kaugus / kiirus;
+        console.log("Sõidu aeg:", soiduaeg, "tundi");
 
-// Postituste kuvamine
-let postitustearv = 137; 
-let postitusilehekuljel = 10; 
+        // Postituste kuvamine
+        let postitustearv = 137; 
+        let postitusilehekuljel = 10; 
 
-// Arvutame lehekülgede arvu
-let lehekulgi = Math.ceil(postitustearv / postitusilehekuljel);
-console.log("Lehekülgi kokku:", lehekulgi);
+        // Arvutame lehekülgede arvu
+        let lehekulgi = Math.ceil(postitustearv / postitusilehekuljel);
+        console.log("Lehekülgi kokku:", lehekulgi);
 
-// Arvutame postituste arvu viimasel lehel
-let viimaselehepostitused = postitustearv % postitusilehekuljel;
-console.log("Postitusi viimasel lehel:", viimaselehepostitused);
+        // Arvutame postituste arvu viimasel lehel
+        let viimaselehepostitused = postitustearv % postitusilehekuljel;
+        console.log("Postitusi viimasel lehel:", viimaselehepostitused);
 
-// Serveri töökulu
-let voimsus = 400; 
-let elektrihind = 0.0969;
+        // Serveri töökulu
+        let voimsus = 400; 
+        let elektrihind = 0.0969;
 
-// Arvutame serveri voolutarbimise kilovatt-tundides (kWh)
-let voolutarbimine = võimsus / 1000;
-console.log("Voolutarbimine:", voolutarbimine, "kWh");
+        // Arvutame serveri voolutarbimise kilovatt-tundides (kWh)
+        let voolutarbimine = voimsus / 1000;
+        console.log("Voolutarbimine:", voolutarbimine, "kWh");
 
-// Arvutame töökulu eurodes
-let tookulu = voolutarbimine * elektrihind;
-console.log("Töökulu ühe tunni jooksul:", tookulu.tofixed(2), "eurot");
+        // Arvutame töökulu eurodes
+        let tookulu = voolutarbimine * elektrihind;
+        console.log("Töökulu ühe tunni jooksul:", tookulu.toFixed(2), "eurot");
+
+
+
+
+
+
+    </script>
+</body>
+</html>
