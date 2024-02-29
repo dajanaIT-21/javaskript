1717e67b45e461a5d41edca84e9a286dadb0df52
Dajana Mähdi <98746095+dajanaIT-21@users.noreply.github.com>
2024-02-29 19:35:08 +0200

    Update Ülesanne 13.java

--- Ülesanne 13.java
+++ Ülesanne 13.java
@@ -4,8 +4,31 @@
 // innertext = tagastab või määrab tekstisisu, setattribute = võimaldab lisada või muuta HTML elemendi atribuute ning nende väärtusi
 // removeattribute = eemaldab elemendi, getattribute = tagastab HTML elemendi määratud atribuudi väärtuse
 
-const id = document.queryselector("p");
-id.innertext = "timm";
-id.setattribute("attr", "dajana mähdi"); 
-id.removeattribute("id");
-console.log(id.getattribute("attr"))
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JavaScripti kood</title>
+</head>
+<body>
+
+    <p id="paragraph">Siin on üks lõik</p>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            const id = document.querySelector("p");
+            id.innerText = "timm";
+            id.setAttribute("attr", "dajana mähdi"); 
+            id.removeAttribute("id");
+            console.log(id.getAttribute("attr"));
+        });
+
+
+
+
+
+
+    </script>
+</body>
+</html>
