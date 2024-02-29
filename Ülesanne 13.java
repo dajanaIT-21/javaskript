712fa072884a58514fa955342adbafe766e8bcaf
Dajana Mähdi <98746095+dajanaIT-21@users.noreply.github.com>
2024-02-29 02:09:39 +0200

    Update Ülesanne 13.java

--- Ülesanne 13.java
+++ Ülesanne 13.java
@@ -1,6 +1,8 @@
 // Dajana
 // 28.02.2024
 // Ülesanne 13
+// innertext = tagastab või määrab tekstisisu, setattribute = võimaldab lisada või muuta HTML elemendi atribuute ning nende väärtusi
+// removeattribute = eemaldab elemendi, getattribute = tagastab HTML elemendi määratud atribuudi väärtuse
 
 const id = document.queryselector("p");
 id.innertext = "timm";
