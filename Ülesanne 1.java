5417147c2bb8ed0e66213c3f0d75a0dff8f818bf
Dajana Mähdi <98746095+dajanaIT-21@users.noreply.github.com>
2024-02-29 19:31:32 +0200

    Update Ülesanne 1.java

--- Ülesanne 1.java
+++ Ülesanne 1.java
@@ -3,11 +3,39 @@
 // Ülesanne 1
 // typeof = väljastab tüübi
  
-console.log(typeof 4.2);           
-console.log(typeof 'Will Code 4 food'); 
-console.log(typeof "123");      
-console.log(typeof true);       
-console.log(typeof [1, 2, 3]);     
-console.log(typeof FALSE);         
-console.log(typeof 'true');       
-console.log(typeof undefined);    
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JavaScripti kood</title>
+</head>
+<body>
+    <script>
+        var numbers = 4.2;              
+        var tekst1 = 'Will Code 4 food';
+        var tekst2 = "123";             
+        var truevar = true;            
+        var massiiv123 = [1, 2, 3];     
+        var falsevar = false;           
+        var truevartext = 'true';       
+        var funnyone;                   
+
+        
+        console.log(typeof(numbers));
+        console.log(typeof(tekst1));
+        console.log(typeof(tekst2));
+        console.log(typeof(truevar));
+        console.log(typeof(massiiv123));
+        console.log(typeof(falsevar));
+        console.log(typeof(truevartext));
+        console.log(typeof(funnyone));
+
+
+
+
+
+
+    </script>
+</body>
+</html>
