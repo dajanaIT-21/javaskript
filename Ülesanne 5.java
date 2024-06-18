cba7261fd480326215efc5a79f986801344aed63
Dajana Mähdi <98746095+dajanaIT-21@users.noreply.github.com>
2024-06-18 17:17:20 +0300

    Update Ülesanne 5.java

--- Ülesanne 5.java
+++ Ülesanne 5.java
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JavaScripti kood</title>
+    <title>Ülesanne 5</title>
 </head>
 <body>
     <script>
