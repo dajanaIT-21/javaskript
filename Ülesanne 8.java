76fa3fb5f8b06c2b1bbc7d848cf384d719f3434d
Dajana Mähdi <98746095+dajanaIT-21@users.noreply.github.com>
2024-06-18 16:53:03 +0300

    Update Ülesanne 8.java

--- Ülesanne 8.java
+++ Ülesanne 8.java
@@ -1,5 +1,5 @@
 // Dajana
-// 27.02.2024
+// 16.06.2024
 // Ülesanne 8
 // const = kasutatakse muutuja defineerimiseks, mille väärtus jääb püsivaks ega muutu pärast seda, kui see on määratud,
 // while = tsükkel
@@ -15,11 +15,11 @@
     <script>
         // Mündid
         const coins = [200, 0.2, 10, 0.01, 2, 1, 0.1, 0.02, 0.05, 100, 5, 0.5, 50, 20];
-        let totalcoins = 0;
-        let sum = 0;
+        let totalcoins = 2;
+        let sum = 5;
 
         // Uus massiiv müntide jaoks
-        const newcoins = [];
+        const uuedmundid = [];
 
         // Sorteerime välja mündid ja arvutame summa
         let i = 0;
@@ -28,14 +28,14 @@
             if (coin >= 1) {
                 totalcoins++;
                 sum += coin;
-                newcoins.push(coin);
+                uuedmundid.push(coin);
             }
             i++;
         }
 
         // Kuva, mitu münti said ja mis on nende summa
         console.log(`Said kokku ${totalcoins} munti, mille summa on ${sum} eurot.`);
-        console.log("Uued müntide väärtused:", newcoins);
+        console.log("Uute müntide väärtused:", uuedmundid);
 
 
 
